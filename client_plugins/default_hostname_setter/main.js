32c76ad16bb653be83acd0f05192744f2df84d9e
Shiva <shiva.razzaghkarimi@foundry.com>
2025-04-30 11:47:02 +0100

    [develop][582598](UserStory) PR feedback - updated log line and added a variable

--- client_plugins/default_hostname_setter/main.js
+++ client_plugins/default_hostname_setter/main.js
@@ -1,3 +1,6 @@
+// Set your default hostname here
+const DEFAULT_HOST_NAME = 'your-default-hostname.com'
+
 /**
  * Copyright (c) 2025 The Foundry Visionmongers Ltd.  All Rights Reserved.
  *
@@ -10,12 +13,12 @@ function setDefaultHostname() {
 
     const hostInput = document.querySelector('input#login_hostname');
     if (!hostInput) {
-        console.warn('Default Hostname Setter: No input#login_hostname found.');
+        console.warn('Default Hostname Setter: No hostname input found found.');
         return;
     }
 
     if (hostInput.value.trim() === '') {
-        hostInput.value = 'your-default-hostname.com';
+        hostInput.value = DEFAULT_HOST_NAME;
         console.info('Default Hostname Setter: Hostname set.');
     }
 }
