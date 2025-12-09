6b6e5e31fcebbca78b5caf8dcf427cc11d619dac
ben.halling <ben.halling@foundry.com>
2025-12-09 15:22:21 +0000

    handle the base64ness in the test

--- flixpy/flix/lib/types.py
+++ flixpy/flix/lib/types.py
@@ -825,7 +825,7 @@ class Sequence(FlixType):
                     "publish_settings": {
                         "bitrate": "36M",
                     },
-                    "xml_asset_b64": xml_asset.decode("utf-8"),
+                    "xml_asset_b64": xml_asset,
                     **params,
                 },
             )
